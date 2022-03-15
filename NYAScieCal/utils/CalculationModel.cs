717f8a01b54c8a84cc5939a3c2c2bed2dd9e524b
Jonathan Lazar <jonathanlazar17@gmail.com>
2022-03-15 17:07:50 +0800

    Added model class for wrapping operands

---
+++ NYAScieCal/utils/CalculationModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYAScieCal.utils
+{
+    class CalculationModel
+    {
+
+        private string firstOperand, endOperand;
+
+        public CalculationModel(string firstOperand,string endOperand)
+        {
+
+            this.firstOperand = firstOperand;
+            this.endOperand = endOperand;
+
+        }
+
+
+        public string getFirstOperand()
+        {
+
+            return this.firstOperand;
+
+        }
+
+        public string getEndOperand()
+        {
+
+            return this.endOperand;
+
+        }
+    }
+}
