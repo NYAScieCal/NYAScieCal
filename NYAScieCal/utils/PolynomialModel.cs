333c891d6c0a0353e679d2e1465f70a3f4022ba7
jonathanlazar <jonathanlazar17@gmail.com>
2022-03-24 14:53:13 +0800

    Added PolynomialModel class

---
+++ NYAScieCal/utils/PolynomialModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYAScieCal.utils
+{
+
+    internal class PolynomialModel
+    {
+
+        private double[] coefficient;
+        private int degree;
+
+        public PolynomialModel(double[] coefficient,int degree)
+        {
+
+            this.coefficient = coefficient;
+            this.degree=degree;
+
+        }
+
+        public double[] getCoefficent()
+        {
+
+            return this.coefficient;
+
+        }
+
+        public int getDegree()
+        {
+
+            return this.degree;
+
+        }
+
+    }
+}
