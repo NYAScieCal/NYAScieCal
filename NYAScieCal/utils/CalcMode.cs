d3b889d075d972274034e427de8eba13490e2ed9
jonathanlazar <jonathanlazar17@gmail.com>
2022-05-11 09:15:04 +0800

    Added CalcMode class for dual mode

---
+++ NYAScieCal/utils/CalcMode.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NYAScieCal.utils
+{
+    class CalcMode
+    {
+
+        private CalcModeConsts calcMode;
+
+        public void setCalcMode(CalcModeConsts calcMode)
+        {
+
+            this.calcMode = calcMode;
+
+        }
+
+        public CalcModeConsts GetCalcMode()
+        {
+
+            return this.calcMode;
+
+        }
+
+
+
+
+    }
+}
